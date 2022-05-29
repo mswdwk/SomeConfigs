c96253ee935246ae76773247bb2a177ba2eb9a4b
mswdwk <mswdwk@outlook.com>
2022-05-29 14:19:11 +0800

    shell: add method records to install oh-my-zhs in CHIN

--- oh-my-zsh/install-oh-my-zsh.sh
+++ oh-my-zsh/install-oh-my-zsh.sh
@@ -1,1 +1,5 @@
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+#curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+# make sure you can resolve raw.github.com by DNS, or ,you should add the right ip in 
+# file /etc/hosts
+# you can debug curl by 'curl --trace t.log   www.xxx.zzz.url'
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
